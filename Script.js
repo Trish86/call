c521657668e91b64ad8148336b688d742df78065
Trish86 <105237555+Trish86@users.noreply.github.com>
2022-06-24 00:29:46 +0100

    initial commit

---
+++ Script.js
@@ -0,0 +1,12 @@
+var n, h, c
+n = prompt("Enter name")
+h = prompt("Enter height")
+c = prompt("Enter country")
+document.write( "Hi," + " " + "My Name is" + " " +n +"." + " " + "I am " + " " +  h + " " + "foot tall." 
++ " " + " I am from" + " "+ c)
+
+
+
+
+
+
